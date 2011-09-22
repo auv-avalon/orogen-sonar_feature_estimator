59a90a82ba687c6a738e74b8bd3ce733a694996b
Sascha Arnold <sascha.arnold@dfki.de>
2011-09-22 20:41:55 +0200

    inital commit

---
+++ tasks/Task.cpp
@@ -0,0 +1,83 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "Task.hpp"
+
+using namespace sonar_feature_estimator;
+
+Task::Task(std::string const& name, TaskCore::TaskState initial_state)
+    : TaskBase(name, initial_state)
+{
+}
+
+Task::Task(std::string const& name, RTT::ExecutionEngine* engine, TaskCore::TaskState initial_state)
+    : TaskBase(name, engine, initial_state)
+{
+}
+
+Task::~Task()
+{
+}
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See Task.hpp for more detailed
+// documentation about them.
+
+// bool Task::configureHook()
+// {
+//     if (! TaskBase::configureHook())
+//         return false;
+//     return true;
+// }
+
+bool Task::startHook()
+{
+    if (! TaskBase::startHook())
+         return false;
+        
+    // check if input ports are connected
+    if (!_sonar_input.connected())
+    {
+        std::cerr << TaskContext::getName() << ": " 
+                    << "Input port 'sonar_input' is not connected." << std::endl;
+        return false;
+    }
+    if (!_orientation_sample.connected())
+    {
+        std::cerr << TaskContext::getName() << ": "
+                    << "Input port 'orientation_sample' is not connected." << std::endl;
+        return false;
+    }
+    
+    return true;
+}
+
+void Task::updateHook()
+{
+    // read current orientation
+    _orientation_sample.readNewest(current_orientation);
+    
+    base::samples::SonarBeam sonarBeam;
+    while (_sonar_input.read(sonarBeam) == RTT::NewData) 
+    {
+        
+    }
+    
+    
+    
+}
+
+// void Task::errorHook()
+// {
+//     TaskBase::errorHook();
+// }
+// void Task::stopHook()
+// {
+//     TaskBase::stopHook();
+// }
+// void Task::cleanupHook()
+// {
+//     TaskBase::cleanupHook();
+// }
+
