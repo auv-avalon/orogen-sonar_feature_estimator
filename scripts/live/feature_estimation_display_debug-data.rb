51789698cfdb9ad20699eef74e16e160c09e15aa
Sascha Arnold <sascha.arnold@dfki.de>
2012-06-28 17:20:38 +0200

    use rock 2d plot instead of qwt

--- scripts/live/feature_estimation_display_debug-data.rb
+++ scripts/live/feature_estimation_display_debug-data.rb
@@ -1,6 +1,5 @@
 require 'orocos'
 require 'Qt4'
-require 'qwt'
 require 'vizkit'
 include Orocos
 
@@ -8,125 +7,51 @@ include Orocos
 Orocos::CORBA.name_service = "127.0.0.1"
 Orocos.initialize
 
-plot = Qwt::Plot.new
-plot.setAxisScale(0,-100,200)
-plot.setAxisScale(2,0,600)
-window_size = plot.minimumSize
-window_size.height = 600
-window_size.width = 800
-plot.minimumSize = window_size
-plot.show
+plotter = Vizkit.default_loader.Plot2d
+#plotter.setZoomAble(true)
+#plotter.setRangeAble(true)
+plotter.show
 
-curve2 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.red = 255
-pen.color = color
-curve2.pen = pen
-curve2.attach(plot)
-curve2.show
-
-curve3 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.red = 255
-color.green = 255
-pen.color = color
-curve3.pen = pen
-curve3.attach(plot)
-curve3.show
-
-curve4 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.green = 255
-color.blue = 255
-pen.color = color
-curve4.pen = pen
-curve4.attach(plot)
-curve4.show
-
-curve5 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.red = 255
-color.blue = 255
-pen.color = color
-curve5.pen = pen
-curve5.attach(plot)
-curve5.show
-
-marker_f1 = Qwt::PlotMarker.new
-marker_f1.lineStyle = 2
-marker_f1.attach(plot)
-marker_f1.show
-
-marker_ground = Qwt::PlotMarker.new
-marker_ground.lineStyle = 2
-pen = Qt::Pen.new
-color = pen.color
-color.green = 128
-pen.color = color
-marker_ground.linePen=pen
-marker_ground.attach(plot)
-marker_ground.show
-
-marker_surface = Qwt::PlotMarker.new
-marker_surface.lineStyle = 2
-pen = Qt::Pen.new
-color = pen.color
-color.blue = 255
-pen.color = color
-marker_surface.linePen=pen
-marker_surface.attach(plot)
-marker_surface.show  
+marker_height = 200
 
 feature_estimator = Orocos::TaskContext.get 'sonar_feature_estimator'
 feature_estimator.enable_debug_output = true
 
 con = Vizkit.connect_port_to 'sonar_feature_estimator', 'debug_output', :pull => false, :update_frequency => 33 do |sample, name|
-    a = Array.new(sample.filteredBeam.length)
-    b = Array.new(sample.filteredBeam.length)
-    for i in 0..sample.filteredBeam.length-1 do
-        a[i] = i
-        b[i] = sample.filteredBeam[i]
-    end
-    curve2.setData(a,b)
-    
-    c = Array.new(sample.device_noise_distribution.length)
-    for i in 0..sample.device_noise_distribution.length-1 do
-        c[i] = sample.device_noise_distribution[i]
-    end
-    curve3.setData(a,c)
-    
-    d = Array.new(sample.gaussian_distribution.length)
-    for i in 0..sample.gaussian_distribution.length-1 do
-        d[i] = sample.gaussian_distribution[i]
-    end
-    curve4.setData(a,d)
-    
-    e = Array.new(sample.derivative.length)
-    for i in 0..sample.derivative.length-1 do
-        e[i] = sample.derivative[i]
-    end
-    curve5.setData(a,e)
+    plotter.update_vector sample.filteredBeam,"filteredBeam"
+    plotter.update_vector sample.device_noise_distribution,"device_noise_distribution"
+    plotter.update_vector sample.gaussian_distribution,"gaussian_distribution"
+    plotter.update_vector sample.derivative,"derivative"
     
     if (sample.bestPos >= 0) then
-        marker_f1.setValue(sample.bestPos,sample.filteredBeam[sample.bestPos])
+        bestPos = Array.new(sample.bestPos + 1)
+        bestPos[sample.bestPos] = marker_height
+        plotter.update_vector bestPos,"bestPos"
     else
-        marker_f1.setValue(-1,0)
+        bestPos = Array.new(1)
+        bestPos[0] = marker_height
+        plotter.update_vector bestPos,"bestPos"
     end
+
     if (sample.pos_surface >= 0) then
-        marker_surface.setValue(sample.pos_surface,1)
+        pos_surface = Array.new(sample.pos_surface + 1)
+        pos_surface[sample.pos_surface] = marker_height
+        plotter.update_vector pos_surface,"pos_surface"
     else
-        marker_surface.setValue(-1,0)
+        pos_surface = Array.new(1)
+        pos_surface[0] = marker_height
+        plotter.update_vector pos_surface,"pos_surface"
     end
+
     if (sample.pos_ground >= 0) then
-        marker_ground.setValue(sample.pos_ground,1)
+        pos_ground = Array.new(sample.pos_ground + 1)
+        pos_ground[sample.pos_ground] = marker_height
+        plotter.update_vector pos_ground,"pos_ground"
     else
-        marker_ground.setValue(-1,0)
+        pos_ground = Array.new(1)
+        pos_ground[0] = marker_height
+        plotter.update_vector pos_ground,"pos_ground"
     end
-    plot.replot
 end 
 
 Vizkit.exec 
--- scripts/replay/feature_estimation_display_debug-data.rb
+++ scripts/replay/feature_estimation_display_debug-data.rb
@@ -1,6 +1,5 @@
 require 'orocos'
 require 'Qt4'
-require 'qwt'
 require 'vizkit'
 include Orocos
 Orocos.initialize
@@ -10,88 +9,12 @@ if ARGV[0] == nil
 	exit
 end
 
+plotter = Vizkit.default_loader.Plot2d
+#plotter.setZoomAble(true)
+#plotter.setRangeAble(true)
+plotter.show
 
-plot = Qwt::Plot.new
-plot.setAxisScale(0,-100,200)
-plot.setAxisScale(2,0,600)
-window_size = plot.minimumSize
-window_size.height = 600
-window_size.width = 800
-plot.minimumSize = window_size
-plot.show
-
-curve = Qwt::PlotCurve.new
-curve.attach(plot)
-curve.show
-
-curve2 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.red = 255
-pen.color = color
-curve2.pen = pen
-curve2.attach(plot)
-curve2.show
-
-curve3 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.red = 255
-color.green = 255
-pen.color = color
-curve3.pen = pen
-curve3.attach(plot)
-curve3.show
-
-curve4 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.green = 255
-color.blue = 255
-pen.color = color
-curve4.pen = pen
-curve4.attach(plot)
-curve4.show
-
-curve5 = Qwt::PlotCurve.new
-pen = Qt::Pen.new
-color = pen.color
-color.red = 255
-color.blue = 255
-pen.color = color
-curve5.pen = pen
-curve5.attach(plot)
-curve5.show
-
-marker_f1 = Qwt::PlotMarker.new
-marker_f1.lineStyle = 2
-marker_f1.attach(plot)
-marker_f1.show
-
-marker_ground = Qwt::PlotMarker.new
-marker_ground.lineStyle = 2
-pen = Qt::Pen.new
-color = pen.color
-color.green = 128
-pen.color = color
-marker_ground.linePen=pen
-marker_ground.attach(plot)
-marker_ground.show
-
-marker_surface = Qwt::PlotMarker.new
-marker_surface.lineStyle = 2
-pen = Qt::Pen.new
-color = pen.color
-color.blue = 255
-pen.color = color
-marker_surface.linePen=pen
-marker_surface.attach(plot)
-marker_surface.show
-
-
-view3d = Vizkit.default_loader.create_widget('vizkit::Vizkit3DWidget')
-view3d.show
-sonarbeamviz = view3d.createPlugin("sonarbeam","SonarBeamVisualization")    
+marker_height = 200
 
 log = Orocos::Log::Replay.open(ARGV)
 sonar_port = log.find_port("/base/samples/SonarBeam")
@@ -100,68 +23,54 @@ Orocos.run 'sonar_feature_estimator_test' do
 
     feature_estimator = Orocos::TaskContext.get 'sonar_feature_estimator'
     feature_estimator.enable_debug_output = true
+    feature_estimator.derivative_history_length = 3
     
     sonar_port.connect_to feature_estimator.sonar_input, :type => :buffer, :size => 100
     #log.sonar.SonarScan.connect_to feature_estimator.sonar_input
     log.orientation_estimator.orientation_samples.connect_to feature_estimator.orientation_sample
     #log.pose_estimator.pose_samples.connect_to feature_estimator.orientation_sample
 
-
     con = Vizkit.connect_port_to 'sonar_feature_estimator', 'debug_output', :pull => false, :update_frequency => 33 do |sample, name|
-        a = Array.new(sample.filteredBeam.length)
-        b = Array.new(sample.filteredBeam.length)
-        for i in 0..sample.filteredBeam.length-1 do
-            a[i] = i
-            b[i] = sample.filteredBeam[i]
-        end
-        curve2.setData(a,b)
         
-        c = Array.new(sample.device_noise_distribution.length)
-        for i in 0..sample.device_noise_distribution.length-1 do
-            c[i] = sample.device_noise_distribution[i]
-        end
-        curve3.setData(a,c)
-        
-        d = Array.new(sample.gaussian_distribution.length)
-        for i in 0..sample.gaussian_distribution.length-1 do
-            d[i] = sample.gaussian_distribution[i]
-        end
-        curve4.setData(a,d)
-        
-        e = Array.new(sample.derivative.length)
-        for i in 0..sample.derivative.length-1 do
-            e[i] = sample.derivative[i]
-        end
-        curve5.setData(a,e)
+        plotter.update_vector sample.filteredBeam,"filteredBeam"
+        plotter.update_vector sample.device_noise_distribution,"device_noise_distribution"
+        plotter.update_vector sample.gaussian_distribution,"gaussian_distribution"
+        plotter.update_vector sample.derivative,"derivative"
         
         if (sample.bestPos >= 0) then
-            marker_f1.setValue(sample.bestPos,sample.filteredBeam[sample.bestPos])
+            bestPos = Array.new(sample.bestPos + 1)
+            bestPos[sample.bestPos] = marker_height
+            plotter.update_vector bestPos,"bestPos"
         else
-            marker_f1.setValue(-1,0)
+            bestPos = Array.new(1)
+            bestPos[0] = marker_height
+            plotter.update_vector bestPos,"bestPos"
         end
+
         if (sample.pos_surface >= 0) then
-            marker_surface.setValue(sample.pos_surface,1)
+            pos_surface = Array.new(sample.pos_surface + 1)
+            pos_surface[sample.pos_surface] = marker_height
+            plotter.update_vector pos_surface,"pos_surface"
         else
-            marker_surface.setValue(-1,0)
+            pos_surface = Array.new(1)
+            pos_surface[0] = marker_height
+            plotter.update_vector pos_surface,"pos_surface"
         end
+
         if (sample.pos_ground >= 0) then
-            marker_ground.setValue(sample.pos_ground,1)
+            pos_ground = Array.new(sample.pos_ground + 1)
+            pos_ground[sample.pos_ground] = marker_height
+            plotter.update_vector pos_ground,"pos_ground"
         else
-            marker_ground.setValue(-1,0)
+            pos_ground = Array.new(1)
+            pos_ground[0] = marker_height
+            plotter.update_vector pos_ground,"pos_ground"
         end
-        plot.replot
+
     end 
 
     sonar_port.connect_to do |sample, null|
-        plot.setAxisScale(2,0,sample.beam.length)
-        a = Array.new(sample.beam.length)
-        b = Array.new(sample.beam.length)
-        for i in 0..sample.beam.length-1 do
-            a[i] = i
-            b[i] = sample.beam[i]
-        end
-        curve.setData(a,b)
-        sonarbeamviz.updateSonarBeam(sample)
+        plotter.update_sonar_beam sample,"sonarBeam"
         sample
     end 
 
